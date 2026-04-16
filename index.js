ae3a7cc83fd9e290cc278cda3f0bbf67b0363062
Gustavo <gustavo.magalhaes_@hotmail.com>
2026-04-16 19:25:13 -0300

    adiciona estrutura base da API

--- index.js
+++ index.js
@@ -7,6 +7,27 @@ app.get('/', (req, res) => {
   res.send('API rodando 🚀');
 });
 
+// lista fake
+let usuarios = [
+  { id: 1, nome: 'Gustavo' }
+];
+
+// GET
+app.get('/usuarios', (req, res) => {
+  res.json(usuarios);
+});
+
+// POST
+app.post('/usuarios', (req, res) => {
+  const novo = {
+    id: usuarios.length + 1,
+    nome: req.body.nome
+  };
+
+  usuarios.push(novo);
+  res.status(201).json(novo);
+});
+
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
 });
