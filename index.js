4709aa30574fc13667ed2173f4b486b3ab1a4296
Gustavo <gustavo.magalhaes_@hotmail.com>
2026-04-26 20:02:18 -0300

    Configurando testes unitarios + CI

--- index.js
+++ index.js
@@ -4,15 +4,13 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('API funcionando com Express 🚀');
+  res.send('API funcionando com Express');
 });
 
-// lista fake
 let usuarios = [
   { id: 1, nome: 'Gustavo' }
 ];
 
-// GET
 app.get('/usuarios', (req, res) => {
   res.json(usuarios);
 });
@@ -21,8 +19,11 @@ app.get('/status', (req, res) => {
   res.json({ status: 'ok' });
 });
 
-// POST
 app.post('/usuarios', (req, res) => {
+  if (!req.body.nome) {
+    return res.status(400).json({ erro: 'Nome é obrigatório' });
+  }
+
   const novo = {
     id: usuarios.length + 1,
     nome: req.body.nome
@@ -32,10 +33,12 @@ app.post('/usuarios', (req, res) => {
   res.status(201).json(novo);
 });
 
-if (!req.body.nome) {
-  return res.status(400).json({ erro: 'Nome é obrigatório' });
+const PORT = 3000;
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log('Servidor rodando na porta 3000');
+  });
 }
 
-app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000');
-});
+module.exports = app;
